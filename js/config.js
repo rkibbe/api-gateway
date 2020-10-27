c2e8f13578f21cc32dd6e0ee4dfec525215a0c50
rkibbe <rkib@excite.com>
2020-10-27 08:18:05 -0400

    Update config.js
    ADD COGNITO User Pool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_7POAKNf9i', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2qluo9im7gmkbh7m7vb56s656e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'US-EAST-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
